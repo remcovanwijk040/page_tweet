927875fff4c8500be324a25dafe7b8196a997bc3
remcovanwijk040 <113189038+remcovanwijk040@users.noreply.github.com>
2024-11-10 22:49:37 +0000

    secret

--- page_tweet.py
+++ page_tweet.py
@@ -42,7 +42,7 @@ prompt_builder = PromptBuilder(template=prompt_template)
 
 llm = OpenAIGenerator(
     #api_key=Secret.from_env_var("XAI_API_KEY"),
-    api_key=st.secrets["XAI_API_KEY"],
+    api_key=st.secrets["api"]["XAI_API_KEY"]
     api_base_url="https://api.x.ai/v1",
     model="grok-beta",
 )
