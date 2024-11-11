be78351369a2abb5c490f2961e988dbdec220fc8
remcovanwijk040 <113189038+remcovanwijk040@users.noreply.github.com>
2024-11-11 10:09:58 +0000

    secret

--- page_tweet.py
+++ page_tweet.py
@@ -14,7 +14,7 @@ import os
 
 # Access the API key from the environment variable
 # api_key = os.getenv("XAI_API_KEY")
-api_key_st = st.secrets["XAI_API_KEY"]
+# api_key_st = st.secrets["XAI_API_KEY"]
 fetcher = LinkContentFetcher()
 converter = HTMLToDocument()
 prompt_template = """
@@ -42,7 +42,7 @@ prompt_builder = PromptBuilder(template=prompt_template)
 
 llm = OpenAIGenerator(
     #api_key=Secret.from_env_var("XAI_API_KEY"),
-    api_key=st.secrets[api_key_st],
+    api_key=st.secrets["XAI_API_KEY"],
     api_base_url="https://api.x.ai/v1",
     model="grok-beta",
 )
